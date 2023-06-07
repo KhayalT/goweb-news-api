78748ee8ea5290dfc792bc8b8fdcb2b9a179624a
khayalteyyubov <teyyubovxeyal04@gmail.com>
2023-06-08 02:03:17 +0400

    Modified NewsResource.php for more accurate response

--- app/Http/Resources/NewsResource.php
+++ app/Http/Resources/NewsResource.php
@@ -2,21 +2,35 @@
 
 namespace App\Http\Resources;
 
-use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-use JsonSerializable;
 
+/**
+ * @property integer $id
+ * @property string $title
+ * @property string $description
+ * @property boolean $status
+ * @property integer $view_count
+ * @property \DateTime $created_at
+ */
 class NewsResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
      *
      * @param  Request  $request
-     * @return array|Arrayable|JsonSerializable
+     * @return array
      */
-    public function toArray($request)
+    public function toArray($request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'status' => $this->status,
+            'view_count' => $this->view_count,
+            'created_at' => $this->created_at->diffForHumans(),
+            'translations' => $this->translations
+        ];
     }
 }
