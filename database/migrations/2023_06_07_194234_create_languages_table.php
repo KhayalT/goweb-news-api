3257b4d2245046184992abe083599125d0ff2110
khayalteyyubov <teyyubovxeyal04@gmail.com>
2023-06-08 00:08:17 +0400

    Added migrations and models

---
+++ database/migrations/2023_06_07_194234_create_languages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLanguagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('languages', function (Blueprint $table) {
+            $table->id();
+            $table->string('iso_code')->unique();
+        });
+
+        Artisan::call('db:seed --class=LanguagesTableSeeder');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('languages');
+    }
+}
---
+++ database/migrations/2023_06_07_194436_create_news_translations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewsTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news_translations', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('news_id');
+            $table->string('title');
+            $table->text('description');
+            $table->unsignedBigInteger('language_id');
+            $table->timestamps();
+
+            $table->foreign('language_id')->references('id')->on('languages')->onDelete('cascade');
+            $table->foreign('news_id')->references('id')->on('news')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news_translations');
+    }
+}
